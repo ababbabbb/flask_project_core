eba37f18bb63ce73735f2fb3ee70b571ad2a1db4
ababbabbb <17826066203@163.com>
2024-05-16 01:48:56 +0800

    修正并打包为第三方扩展

---
+++ example/main.py
@@ -0,0 +1,19 @@
+from flask_projects.convension import project
+from flask_project_restful.container import RestfulContainer
+from flask_project_restful.logic import RestfulLogic
+from flask_project_restful.scanner import RestfulScanner
+
+
+project.setter_args(
+    ip='0.0.0.0',
+    port='8080',
+    path=__file__
+).setter_extend(
+    RestfulScanner,
+    RestfulContainer,
+    RestfulLogic
+)
+
+
+if __name__ == '__main__':
+    project.byOrder()
