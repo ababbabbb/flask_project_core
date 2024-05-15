c72c12911163eaf332f2adc7da9db7d996a629c6
ababbabbb <17826066203@163.com>
2024-05-16 00:47:47 +0800

    增加第三方包打包代码

--- tamp/extends/restful/container.py
+++
@@ -1,6 +0,0 @@
-from default.router.plan_0.container import Plan0RouterContainer
-
-
-class RestfulContainer(Plan0RouterContainer):
-
-    ...
--- tamp/extends/restful/logic.py
+++
@@ -1,6 +0,0 @@
-from default.router.plan_0.logic import Plan0RouterLogic
-
-
-class RestfulLogic(Plan0RouterLogic):
-
-    ...
