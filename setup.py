8cd62a3f5e8eafdf9d5e3490dc9478377a1252f5
ababbabbb <17826066203@163.com>
2024-05-18 18:52:27 +0800

    修改了打包文件

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 # you need to change all these
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = "flask projects系列快速开发手脚架核心"
 
 setup(
